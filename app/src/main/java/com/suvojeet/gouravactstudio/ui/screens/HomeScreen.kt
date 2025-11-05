3c8aafd34b97cfeb0dbdd3d71cb076ed9a76741f
suvojeet-sengupta <suvojitsengupta21@gmail.com>
2025-11-06 00:23:13 +0530

    Fix: Restore missing imports in HomeScreen.kt

--- app/src/main/java/com/suvojeet/gouravactstudio/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/suvojeet/gouravactstudio/ui/screens/HomeScreen.kt
@@ -1,6 +1,29 @@
+package com.suvojeet.gouravactstudio.ui.screens
+
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.suvojeet.gouravactstudio.Screen
+import com.suvojeet.gouravactstudio.ui.components.AppLogo
+import com.suvojeet.gouravactstudio.ui.theme.GouravActStudioTheme
 
 @Composable
 fun HomeScreen(navController: NavController, modifier: Modifier = Modifier) {
