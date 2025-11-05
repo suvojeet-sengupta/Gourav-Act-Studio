8c4f87867f659375a108ba003e9b7a7ebcd5cc05
suvojeet-sengupta <suvojitsengupta21@gmail.com>
2025-11-06 00:17:52 +0530

    Fix: App crash due to unsupported drawable

--- app/src/main/java/com/suvojeet/gouravactstudio/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/suvojeet/gouravactstudio/ui/screens/HomeScreen.kt
@@ -1,40 +1,21 @@
-package com.suvojeet.gouravactstudio.ui.screens
-
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
-import com.suvojeet.gouravactstudio.R
-import com.suvojeet.gouravactstudio.Screen
-import com.suvojeet.gouravactstudio.ui.components.AppLogo
-import com.suvojeet.gouravactstudio.ui.theme.GouravActStudioTheme
+import androidx.compose.foundation.background
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun HomeScreen(navController: NavController, modifier: Modifier = Modifier) {
-    Box(modifier = modifier.fillMaxSize()) {
-        Image(
-            painter = painterResource(id = R.drawable.home_background),
-            contentDescription = "Background",
-            contentScale = ContentScale.Crop,
-            modifier = Modifier.fillMaxSize()
-        )
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Color.White,
+                        Color(0xFFE0E0E0)
+                    )
+                )
+            )
+    ) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
