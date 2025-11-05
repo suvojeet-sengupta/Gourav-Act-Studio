3e9b2ff67eafe0d412d8f22df72502f770b88958
suvojeet-sengupta <suvojitsengupta21@gmail.com>
2025-11-05 23:55:19 +0530

    Fix: Status bar color and icon visibility

--- app/src/main/java/com/suvojeet/gouravactstudio/ui/theme/Theme.kt
+++ app/src/main/java/com/suvojeet/gouravactstudio/ui/theme/Theme.kt
@@ -52,8 +52,8 @@ fun GouravActStudioTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.statusBarColor = colorScheme.background.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
