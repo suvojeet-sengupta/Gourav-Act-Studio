6d62fd71be1175d46f7cd5eeb974d4f91bd687ac
suvojeet-sengupta <suvojitsengupta21@gmail.com>
2025-11-07 23:14:07 +0530

    fix(build): Add missing clickable import

--- app/src/main/java/com/suvojeet/gauravactstudio/ui/screens/CreateOwnPackage.kt
+++ app/src/main/java/com/suvojeet/gauravactstudio/ui/screens/CreateOwnPackage.kt
@@ -1,6 +1,7 @@
 package com.suvojeet.gauravactstudio.ui.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
