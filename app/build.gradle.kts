df49adc4b605c1dce1aef191a17750ab8149d7b7
suvojeet-sengupta <suvojitsengupta21@gmail.com>
2026-01-19 14:17:12 +0530

    Bump version code to 7 and version name to 1.2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.suvojeet.gauravactstudio"
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.2.2"
+        versionCode = 7
+        versionName = "1.2.3"
         resConfigs("en", "hi")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
